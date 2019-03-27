91229c6ed43978d26f09478ed7ee3a7b41125fbf
0x2019 <48742368+0x2019@users.noreply.github.com>
2019-03-27 11:06:41 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 // For command line git tutorial
-// First: Local repository modification.
-// Second: GitHub modification
+// First: GitHub modification
+// Second: Local repository modification
 
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
